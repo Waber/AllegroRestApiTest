0f1e44e4403c23d9ea52cc5631ac310399f3b192
Przemyslaw Waberski <przemekwaberski@gmail.com>
2021-04-01 12:42:11 +0200

    Removed personel client and secret Id

--- src/main/java/testBase/TestBase.java
+++ src/main/java/testBase/TestBase.java
@@ -7,8 +7,8 @@ import org.testng.annotations.BeforeMethod;
 import java.lang.reflect.Method;
 
 public class TestBase {
-    private String clientId = "d693faf0a35045b8b2535a295a4307fa";
-    private String clientSecretId = "82YM1jaNebM6K4vX2kaYTxjlpw5Q2xaI7mD8px1zH75X7RpdOqDmWgm6CPOAxUbE";
+    private String clientId = "Insert your clientId";
+    private String clientSecretId = "Insert your client secret Id";
     public ApiConnection apiConnection;
     public ApiDataService apiDataService;
     private String token;
